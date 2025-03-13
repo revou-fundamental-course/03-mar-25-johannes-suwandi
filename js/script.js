24192ea28c254b638a2dc2b36d44e780f7086421
Johannes <jsuwand@gmail.com>
2025-03-14 06:45:40 +0700

    Fixing banner autoslide and display responsive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-// Ini file javascript
-
-
 // Banner autoslide
 let slideIndex = 0;
 showSlides();
@@ -19,7 +16,7 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
 
 
