27a6af00af6c125a67d950140aa55fea7b2ca563
Johannes <jsuwand@gmail.com>
2025-03-13 20:20:22 +0700

    Added a function for message-me

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // Ini file javascript
+
+function displayMessage() {
+    document.getElementById("currentTime").innerText = new Date().toString();
+    document.getElementById("outName").innerText = document.getElementById("name").value;
+    document.getElementById("outDob").innerText = document.getElementById("dob").value;
+    let gender = document.querySelector('input[name="gender"]:checked');
+    document.getElementById("outGender").innerText = gender ? gender.value : "";
+    document.getElementById("outMessage").innerText = document.getElementById("message").value;
+}
