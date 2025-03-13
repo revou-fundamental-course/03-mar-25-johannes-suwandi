6524142400c02f170bff2659c19dd7f84fb8e934
Johannes <jsuwand@gmail.com>
2025-03-13 23:26:48 +0700

    Added banner autoslide feature

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 // Ini file javascript
 
+
+// Banner autoslide
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+// Message me section
 function displayMessage() {
     document.getElementById("currentTime").innerText = new Date().toString();
     document.getElementById("outName").innerText = document.getElementById("name").value;
